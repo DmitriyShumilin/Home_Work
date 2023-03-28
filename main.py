fcbf28ec84a490ad1b246496ff012ba2703de084
DmitriyShumilin <shumilin.dima2018@gmail.com>
2023-03-28 20:51:41 +0300

    comment

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-string = "abcdefghi"
-string = str.capitalize(string) # string = "Abcdefghi"
-print(string)
-string = str.upper(string) # string = "ABCDEFGHI"
-print(string)
-string = "abcdefghi"
-print(string[:4])
-string = "abcdefghi"
-print(string.translate({ord(i): None for i in "acegi"}))
-
+a = [7, 10, 23, -19, 80]
+a.sort()
+print(a)
+a.extend([20, 21])
+print(a)
+b = sorted(a)
+print(b)
+a = a[1::2]
+print(a)
+a = [-19, 7, 10, 20, 21, 23, 80]
+print(a.count(20))
+item = 20
+index = a.index(item)
+print(index)
+a = [-19, 7, 10, 20, 21, 23, 80]
+a[3] = 19
+print(a)
 
