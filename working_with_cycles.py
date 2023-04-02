819a3021b44a02c0a12aa74ff7b84c031f95d41b
Dima <shumilin.dima2018@gmail.com>
2023-04-02 09:33:47 +0300

    init commit

---
+++ working_with_cycles.py
@@ -0,0 +1,11 @@
+for number in range (0, 100):
+    if (number % 4 == 0):
+        print (number)
+dict_ ={"name": "KirilL", "age": 44}
+for item in dict_.items():
+    print(item[0], item[1])
+x = int(input('Введите число:'))
+res = 0
+for i in range(1, x + 1):
+    res += i
+print(res)
