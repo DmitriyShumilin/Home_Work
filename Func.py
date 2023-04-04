67ade3bbb4521c4450604d0cbe9dd307e3268b35
Dima <shumilin.dima2018@gmail.com>
2023-04-04 20:36:33 +0300

    init commit

---
+++ Func.py
@@ -0,0 +1,34 @@
+#      #Палиндромы
+# def isPalindrome(x):
+#     rev = ''.join(reversed(x))
+#     if (x == rev):
+#         return True
+#         return False
+# x = "madam"
+# y = isPalindrome(x)
+# if (y):
+#     print("Yes")
+# else:
+#     print("No")
+#
+#     #Сколько раз встречаеться символ в строке?
+# my_list = 'bootlegger'
+# count = my_list.count('g')
+# print("Count of g in  is : 'bootlegger:" + str(count))
+
+    #Функции
+# def func(*list):
+#     total_list = []
+#     for i in list:
+#         total_list += i
+#     print(total_list)
+#
+# func(['a', 'b', 'c'], ['d', 'e', 'f'])
+
+
+# def dict_(my_dict):
+#     dict_test = {value: key for key, value in my_dict.items()}
+#     print(dict_test)
+
+
+# dict_({'Name': 'Dmitriy', 'Age': 33})
