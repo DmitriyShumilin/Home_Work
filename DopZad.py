f07cf3efd13c9d6790601799a4ba55b50b4c2eb7
Dima <shumilin.dima2018@gmail.com>
2023-04-05 11:45:31 +0300

    init commit

---
+++ DopZad.py
@@ -0,0 +1,11 @@
+# def foo(meaning):
+#     if meaning == 'str_snake_case':
+#         meaning_new = meaning.split('_')
+#         meaning_str = ' '.join(meaning_new).title()
+#         w_camel = meaning_str.replace('snake', 'camel').replace(' ', '_')
+#         print(w_camel)
+#     else:
+#         print('Ошибка ввода.')
+#
+#
+# foo('str_snake_case')
